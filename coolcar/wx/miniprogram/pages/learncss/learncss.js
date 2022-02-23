8017ff95a82e021a110a3b3d1d36c763e2f53002
lizonglin_TNcarrot <lizonglin313@outlook.com>
2022-02-23 17:31:59 +0800

    update

---
+++ coolcar/wx/miniprogram/pages/learncss/learncss.js
@@ -0,0 +1,21 @@
+"use strict";
+Page({
+    data: {
+        showPath: true,
+        values: [
+            {
+                name: 'john',
+                id: 1,
+            },
+            {
+                name: 'marry',
+                id: 2,
+            },
+            {
+                name: 'tom',
+                id: 3,
+            }
+        ],
+    },
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibGVhcm5jc3MuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJsZWFybmNzcy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsSUFBSSxDQUFDO0lBQ0gsSUFBSSxFQUFFO1FBQ0osUUFBUSxFQUFFLElBQUk7UUFDZCxNQUFNLEVBQUU7WUFDTjtnQkFDRSxJQUFJLEVBQUUsTUFBTTtnQkFDWixFQUFFLEVBQUUsQ0FBQzthQUNOO1lBQ0Q7Z0JBQ0UsSUFBSSxFQUFFLE9BQU87Z0JBQ2IsRUFBRSxFQUFFLENBQUM7YUFDTjtZQUNEO2dCQUNFLElBQUksRUFBRSxLQUFLO2dCQUNYLEVBQUUsRUFBRSxDQUFDO2FBQ047U0FDRjtLQUNKO0NBQ0EsQ0FBQyxDQUFBIiwic291cmNlc0NvbnRlbnQiOlsiUGFnZSh7XG4gIGRhdGE6IHtcbiAgICBzaG93UGF0aDogdHJ1ZSxcbiAgICB2YWx1ZXM6IFtcbiAgICAgIHtcbiAgICAgICAgbmFtZTogJ2pvaG4nLFxuICAgICAgICBpZDogMSxcbiAgICAgIH0sXG4gICAgICB7XG4gICAgICAgIG5hbWU6ICdtYXJyeScsXG4gICAgICAgIGlkOiAyLFxuICAgICAgfSxcbiAgICAgIHtcbiAgICAgICAgbmFtZTogJ3RvbScsXG4gICAgICAgIGlkOiAzLFxuICAgICAgfVxuICAgIF0sXG59LFxufSlcbiJdfQ==
