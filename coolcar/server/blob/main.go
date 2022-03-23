6bef9ad220aa16828632af87f0ae0bdff34b8d61
lizonglin_TNcarrot <lizonglin313@outlook.com>
2022-03-23 15:23:57 +0800

    fix: use oss

--- coolcar/server/blob/main.go
+++ coolcar/server/blob/main.go
@@ -1,10 +1,11 @@
-package blob
+package main
 
 import (
 	"context"
 	blobpb "coolcar/blob/api/gen/v1"
 	"coolcar/blob/blob"
 	"coolcar/blob/dao"
+	aliyun_oos "coolcar/blob/oos"
 	"coolcar/shared/server"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -25,6 +26,21 @@ func main() {
 		logger.Fatal("cannot connect mongodb", zap.Error(err))
 	}
 	db := mongoClient.Database("coolcar")
+	// 使用cos
+	//storage, err := cos.NewService(
+	//	"https://coolcar-1258527714.cos.ap-chengdu.myqcloud.com",
+	//	"AKIDkJdhX7SXNyJPJyNWQ5UdXBajdHgpjQt7",
+	//	"nMglaAVaPzJODn7C2cx3tTotjJdiY8SW")
+
+	// 使用 oss
+	storage, err := aliyun_oos.NewService(
+		"oss-cn-beijing.aliyuncs.com",
+		"LTAI5tMHJ7sDiyySLtCsE8bq",
+		"B5Q3tcfRUDnmciH7vPhY54CHI5a67V",
+		"coolcar-lzl")
+	if err != nil {
+		log.Fatal("cannot create cos service", zap.Error(err))
+	}
 
 	logger.Sugar().Fatal(server.RunGRPCServer(&server.GRPCConfig{
 		Name:   "blob",
@@ -32,6 +48,7 @@ func main() {
 		Logger: logger,
 		RegisterFunc: func(s *grpc.Server) {
 			blobpb.RegisterBlobServiceServer(s, &blob.Service{
+				Storage: storage,
 				Mongo:  dao.NewMongo(db),
 				Logger: logger,
 			})
---
+++ coolcar/server/blob/oos/oos.go
@@ -0,0 +1,93 @@
+// 使用 aliyun 接口重新实现对象存储服务
+package aliyun_oos
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type Service struct {
+	client *oss.Client
+	secID  string
+	secKey string
+	endPoint string
+	bucketName string
+}
+
+// SignURL 获取签名的上传或者访问URL.
+func (s *Service) SignURL(c context.Context, method, path string, timeout time.Duration) (string, error) {
+
+	// 获取存储空间。
+	bucket, err := s.client.Bucket(s.bucketName)
+	if err != nil {
+		return "", err
+	}
+	signedURL := ""
+	if method == "GET" {
+		// 这里尝试将 time.Duration 转 int64 的 秒 表示
+		signedURL, err = bucket.SignURL(path, http.MethodGet, int64(timeout.Seconds()), nil)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	if method == "PUT" {
+		signedURL, err = bucket.SignURL(path, http.MethodPut, int64(timeout.Seconds()), nil)
+		if err != nil {
+			return "", err
+		}
+	}
+	// 拿到正确的URL返回
+	return signedURL, nil
+}
+
+
+func (s *Service) Get(c context.Context, path string) (io.ReadCloser, error) {
+
+	// 首先获取 bucket
+	bucket, err := s.client.Bucket(s.bucketName)
+	if err != nil {
+		return nil, err
+	}
+
+	// 获取对象消息流
+	body, err := bucket.GetObject(path, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer body.Close()
+
+	// 从响应中读数据到data
+	data, err := ioutil.ReadAll(body)
+
+	// []byte 转 readCloser
+	rc := ioutil.NopCloser(bytes.NewReader(data))
+
+
+	return rc, nil
+}
+
+func NewService(endpoint, akID, akSec, bucketname string) (*Service, error) {
+
+	client, err := oss.New(endpoint, akID, akSec)
+	if err != nil {
+		return nil, fmt.Errorf("cannot connect to oss storage: %v", err)
+	}
+
+	return &Service{
+		client:    	client,
+		secID:      akID,
+		secKey:     akSec,
+		endPoint:   endpoint,
+		bucketName: bucketname,
+	}, nil
+
+}
+
+
