7d1ef5bbbf6cb349c8224b98947b7b6212c1008d
DKCBBB <40293922+lizonglin313@users.noreply.github.com>
2022-03-27 01:32:12 +0800

    feature: car service

---
+++ coolcar/server/car/car/car.go
@@ -0,0 +1,109 @@
+package car
+
+import (
+	"context"
+	carpb "coolcar/car/api/gen/v1"
+	"coolcar/car/dao"
+	"coolcar/shared/id"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type Service struct {
+	Logger *zap.Logger
+	Mongo  *dao.Mongo
+	carpb.UnimplementedCarServiceServer
+}
+
+func (s *Service) CreateCar(ctx context.Context, request *carpb.CreateCarRequest) (*carpb.CarEntity, error) {
+	cr, err := s.Mongo.CreateCar(ctx)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &carpb.CarEntity{
+		Id:  cr.ID.Hex(),
+		Car: cr.Car,
+	}, nil
+}
+
+func (s *Service) GetCar(ctx context.Context, request *carpb.GetCarRequest) (*carpb.Car, error) {
+	cr, err := s.Mongo.GetCar(ctx, id.CarID(request.Id))
+	if err != nil {
+		return nil, status.Error(codes.NotFound, "")
+	}
+	return cr.Car, nil
+}
+
+func (s *Service) GetCats(ctx context.Context, request *carpb.GetCarsRequest) (*carpb.GetCarsResponse, error) {
+	crs, err := s.Mongo.GetCars(ctx)
+	if err != nil {
+		s.Logger.Error("cannot get cars", zap.Error(err))
+		return nil, status.Error(codes.Internal, "")
+	}
+
+	res := &carpb.GetCarsResponse{}
+	for _, cr := range crs {
+		res.Cars = append(res.Cars, &carpb.CarEntity{
+			Id:  cr.ID.Hex(),
+			Car: cr.Car,
+		})
+	}
+	return res, nil
+}
+
+func (s *Service) LockCar(ctx context.Context, request *carpb.LockCarRequest) (*carpb.LockCarResponse, error) {
+	// 注意 锁车的前置条件是： 车是处于 unlock状态的
+	err := s.Mongo.UpdateCar(ctx, id.CarID(request.Id), carpb.CarStatus_UNLOCKED, &dao.CarUpdate{
+		Status: carpb.CarStatus_LOCKING,
+	})
+	if err != nil {
+		code := codes.Internal
+		if err == mongo.ErrNoDocuments {
+			code = codes.NotFound
+		}
+		return nil, status.Errorf(code, "cannot update %v", err)
+	}
+	return &carpb.LockCarResponse{}, nil
+}
+
+func (s *Service) UnlockCar(ctx context.Context, request *carpb.UnlockCarRequest) (*carpb.UnlockCarResponse, error) {
+	err := s.Mongo.UpdateCar(ctx, id.CarID(request.Id), carpb.CarStatus_LOCKED, &dao.CarUpdate{
+		Status:       carpb.CarStatus_UNLOCKING,
+		Driver:       request.Driver,
+		UpdateTripID: true,
+		TripID:       id.TripID(request.TripId),
+	})
+	if err != nil {
+		code := codes.Internal
+		if err == mongo.ErrNoDocuments {
+			code = codes.NotFound
+		}
+		return nil, status.Errorf(code, "cannot update %v", err)
+	}
+	return &carpb.UnlockCarResponse{}, nil
+}
+
+func (s *Service) UpdateCar(ctx context.Context, request *carpb.UpdateCarRequest) (*carpb.UpdateCarResponse, error) {
+	update := &dao.CarUpdate{
+		Status: request.Status,
+		Position: request.Position,
+	}
+
+	// 如果更新的状态是上锁状态 那么就清掉一些信息
+	if request.Status == carpb.CarStatus_LOCKED {
+		update.Driver = &carpb.Driver{}
+		update.UpdateTripID = true
+		update.TripID = id.TripID("")
+	}
+
+	// 前置状态为NOT_SPECIFIED则不追加状态信息为查询条件
+	// 查到什么状态的车都进行更新
+	err := s.Mongo.UpdateCar(ctx, id.CarID(request.Id), carpb.CarStatus_CS_NOT_SPECIFIED, update)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &carpb.UpdateCarResponse{}, nil
+}
---
+++ coolcar/server/car/dao/mongo.go
@@ -0,0 +1,143 @@
+package dao
+
+import (
+	"context"
+	carpb "coolcar/car/api/gen/v1"
+	"coolcar/shared/id"
+	mgo "coolcar/shared/mongo"
+	"coolcar/shared/mongo/objid"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	carField      = "car"
+	statusField   = carField + ".status"
+	driverField   = carField + ".driver"
+	positionField = carField + ".position"
+	tripIDField   = carField + ".tripid"
+)
+
+type CarRecord struct {
+	mgo.IDField `bson:"inline"`
+	Car         *carpb.Car `bson:"car"`
+}
+
+type Mongo struct {
+	col *mongo.Collection // mongo 中的表结构
+}
+
+// NewMongo 根据给出的数据库，从中获取操作的 trip 表.
+func NewMongo(db *mongo.Database) *Mongo {
+	return &Mongo{
+		col: db.Collection("car"),
+	}
+}
+
+func (m *Mongo) CreateCar(ctx context.Context) (*CarRecord, error) {
+	r := &CarRecord{
+		Car: &carpb.Car{
+			Status: carpb.CarStatus_LOCKED,
+			Position: &carpb.Location{
+				Latitude:  30,
+				Longitude: 120,
+			},
+		},
+	}
+	r.ID = mgo.NewObjID()
+	_, err := m.col.InsertOne(ctx, r)
+	if err != nil {
+		return nil, err
+	}
+	return nil, err
+}
+
+func (m *Mongo) GetCar(ctx context.Context, id id.CarID) (*CarRecord, error) {
+	objID, err := objid.FromID(id)
+	if err != nil {
+		return nil, fmt.Errorf("invaild id: %v", err)
+	}
+
+	res := m.col.FindOne(ctx, bson.M{
+		mgo.IDFieldName: objID,
+	})
+	if err := res.Err(); err != nil {
+		return nil, err
+	}
+
+	var cr CarRecord
+	err = res.Decode(&cr)
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode: %v", err)
+	}
+
+	return &cr, nil
+}
+
+func (m *Mongo) GetCars(ctx context.Context) ([]*CarRecord, error) {
+	// 暂时全部选出来
+	filter := bson.M{}
+	res, err := m.col.Find(ctx, filter, options.Find())
+	if err != nil {
+		return nil, err
+	}
+
+	var carRecodes []*CarRecord
+	for res.Next(ctx) {
+		var car CarRecord
+		err := res.Decode(&car)
+		if err != nil {
+			return nil, err
+		}
+		carRecodes = append(carRecodes, &car)
+	}
+	return carRecodes, nil
+}
+
+type CarUpdate struct {
+	Status       carpb.CarStatus
+	Position     *carpb.Location
+	Driver       *carpb.Driver
+	UpdateTripID bool
+	TripID       id.TripID
+}
+
+// UpdateCar 这里的status用来做更新car的前置条件.
+func (m *Mongo) UpdateCar(ctx context.Context, id id.CarID, status carpb.CarStatus, update *CarUpdate) error {
+	objID, err := objid.FromID(id)
+	if err != nil {
+		return fmt.Errorf("invaild car id: %v", err)
+	}
+
+	// 判断以下是 只用 id 来定位哪条记录 还是使用 id+status 来找
+	// 在没有指定状态时 就只用 id 来找
+	filter := bson.M{
+		mgo.IDFieldName: objID,
+	}
+	if status != carpb.CarStatus_CS_NOT_SPECIFIED {
+		filter[statusField] = status
+	}
+
+	// 设置更新内容
+	u := bson.M{}
+	if update.Status != carpb.CarStatus_CS_NOT_SPECIFIED {
+		u[statusField] = update.Status
+	}
+	if update.Driver != nil {
+		u[driverField] = update.Driver
+	}
+	if update.Position != nil {
+		u[positionField] = update.Position
+	}
+
+	res, err := m.col.UpdateOne(ctx, filter, mgo.Set(u))
+	if err != nil {
+		return err
+	}
+	if res.MatchedCount == 0 {
+		return mongo.ErrNoDocuments
+	}
+	return nil
+}
